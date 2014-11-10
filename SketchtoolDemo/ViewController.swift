a8d4de1df84ea65cfbe84b7a29c8d48bf262e462
Liyao Chen <gliyao@gmail.com>
2014-11-10 11:20:50 +0800

    first commit

---
+++ SketchtoolDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SketchtoolDemo
+//
+//  Created by Liyao on 2014/11/10.
+//  Copyright (c) 2014年 swiftTaipei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
